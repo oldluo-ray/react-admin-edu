dc99d2fc189ec0d38f01dc94d01631100f722501
old-luo <luodianlei7@163.com>
2020-07-13 15:49:57 +0800

    函数名更正

--- src/api/edu/chapter.js
+++ src/api/edu/chapter.js
@@ -4,7 +4,7 @@ import request from '@utils/request'
 const BASE_URL = '/admin/edu/chapter'
 
 // 获取所有课程数据
-export function reqGetCourseList({ page, limit, courseId }) {
+export function reqGetChapterList({ page, limit, courseId }) {
   // request返回一个promise
   return request({
     url: `${BASE_URL}/${page}/${limit}`,
