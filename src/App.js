fbddda1ade42cf98eccbcab523ead59bb18c7e67
old-luo <luodianlei7@163.com>
2020-07-17 16:10:46 +0800

    antd国际化

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import history from '@utils/history'
 
 // 导入国际化包
 import { IntlProvider } from 'react-intl'
+
+// antd国际化
+import { ConfigProvider } from 'antd'
+
 //导入pussub
 import PubSub from 'pubsub-js'
 
@@ -12,12 +16,17 @@ import PubSub from 'pubsub-js'
 // zh 中文语言包
 import { en, zh } from './locales'
 
+// 2. 引入antd中提供的语言包
+import enUS from 'antd/es/locale/en_US'
+import zhCN from 'antd/es/locale/zh_CN'
+
 import Layout from './layouts'
 // 引入重置样式（antd已经重置了一部分了）
 import './assets/css/reset.css'
 
 function App() {
   const [locale, setLocale] = useState('zh')
+
   useEffect(() => {
     const token = PubSub.subscribe('LANGUAGE', (messge, data) => {
       console.log(data)
@@ -33,14 +42,17 @@ function App() {
   // const locale = 'en'
   // 根据当前语言环境决定返回什么包
   const message = locale === 'en' ? en : zh
+  const antdLocale = locale === 'en' ? enUS : zhCN
   return (
     <Router history={history}>
-      {/* locale 表示当前语言环境 
+      <ConfigProvider locale={antdLocale}>
+        {/* locale 表示当前语言环境 
           message 表示使用哪个语言包
       */}
-      <IntlProvider locale={locale} messages={message}>
-        <Layout />
-      </IntlProvider>
+        <IntlProvider locale={locale} messages={message}>
+          <Layout />
+        </IntlProvider>
+      </ConfigProvider>
     </Router>
   )
 }
